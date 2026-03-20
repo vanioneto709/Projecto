8b0889fcf4f25d0a15d775a2d9bbefd3b7563d4c
vanioneto709 <vanioneto709@gmail.com>
2026-03-20 10:59:44 +0100

    esqueci uma virgula

--- Ssorrisos/consultas/api_urls.py
+++ Ssorrisos/consultas/api_urls.py
@@ -5,7 +5,7 @@ from .api_views import cadastro_api
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='api_login'),
     path('token/refresh/', TokenRefreshView.as_view()),
-    path('cadastro/', cadastro_api)
+    path('cadastro/', cadastro_api),
     path('me/', me_api),
     path('minhas-consultas/', minhas_consultas_api),
 ]
